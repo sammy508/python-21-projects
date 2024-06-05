96f4f9b25635927c5f9d5192b5bf33f741e0abde
Sammy508 <chapagainsamir9@gmial.com>
2024-06-05 23:32:55 +0545

    madlibs game

---
+++ projects code/medium/madlibs generator/madlibs_generator.py
@@ -0,0 +1,31 @@
+
+with open("story.txt","r") as file:
+    story = file.read()
+    print(story)
+
+words= set()
+
+start_of_word = -1
+
+word_start_targrt = "<"
+word_end_targrt = ">"
+
+
+for i, char in enumerate(story):
+    if char == word_start_targrt:
+        start_of_word = i
+
+    if char == word_end_targrt and start_of_word !=-1:    
+        word = story[start_of_word: i+1]
+        words.add(word)
+        start_of_word = -1
+
+answers ={}
+
+for word in words:
+    answer = input(f"Enter compatible word for {word} :")
+    answers[word] = answer
+print(answers) 
+
+word,answer = answer, word
+print(word)
