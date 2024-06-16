8056062cf8071e54fe4d76069a9f4be19062103a
Sammy508 <chapagainsamir9@gmial.com>
2024-06-16 19:38:38 +0545

    issue on this code

--- projects code/medium/wpm typing/word_type.py
+++ projects code/medium/wpm typing/word_type.py
@@ -1,139 +1,61 @@
 
 
-# #WPm 
-# import curses
-# from curses import wrapper
-# def start_screen(stdscr):
-#     stdscr.clear()
-#     stdscr.addstr("Welcome to the speed typing test!")
-#     stdscr.addstr("\n press any kry to begin! \n")
-#     stdscr.refresh()
-#     key = stdscr.getkey()
-
-
-#     stdscr.refresh()
-#     stdscr.getkey()
-
-
-# def display_text(stdscr,current,target, wpm=0):
-#     stdscr.addstr(target)
-
-#     for i,char in enumerate(current):
-#             stdscr.addstr(char, curses.color_pair(2))
-
-
-# def wpm_tester(stdscr):
-#     target_text = "The quick brown fox jumps over the laxy dog."
-#     current_text =[]
-
-#     while True:
-#         stdscr.clear()
-#         display_text(stdscr,target_text,current_text)
-#         stdscr.refresh()
-#         key = stdscr.getkey()
-
-#         if ord(key)== 27:
-#             break
-#         if key in ("KEY_BACKSPACE", "\b","\x7f"):
-#             if len(current_text)>0:
-#                 current_text.pop()
-#         else:
-#             current_text.append(key) 
-
-
-
-
-
-# def main(stdscr):
-#     curses.init_pair(  1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-#     curses.init_pair(  2, curses.COLOR_WHITE, curses.COLOR_BLACK)
-#     curses.init_pair(  3, curses.COLOR_GREEN, curses.COLOR_WHITE)
-   
-#     start_screen(stdscr)
- 
-#     wpm_tester(stdscr)
-  
-
-
-# wrapper(main) # Wrapper call outside of the main function
-
-
+#WPm 
 import curses
 from curses import wrapper
-import time
-import random
-
-
 def start_screen(stdscr):
-	stdscr.clear()
-	stdscr.addstr("Welcome to the Speed Typing Test!")
-	stdscr.addstr("\nPress any key to begin!")
-	stdscr.refresh()
-	stdscr.getkey()
+    stdscr.clear()
+    #  stdscr.addstr(1,0"Welcome to the speed typing test!") #here 1, 0 means coordination 1 line down and 0 denotes starting space
+    stdscr.addstr("Welcome to the speed typing test!")
+    stdscr.addstr("\n press any kry to begin! \n")
+    stdscr.refresh()
+    key = stdscr.getkey()
 
-def display_text(stdscr, target, current, wpm=0):
-	stdscr.addstr(target)
-	stdscr.addstr(1, 0, f"WPM: {wpm}")
 
-	for i, char in enumerate(current):
-		correct_char = target[i]
-		color = curses.color_pair(1)
-		if char != correct_char:
-			color = curses.color_pair(2)
+    stdscr.refresh()
+    stdscr.getkey() # it waits to user to press some keys
 
-		stdscr.addstr(0, i, char, color)
 
-def load_text():
-	with open("text.txt", "r") as f:
-		lines = f.readlines()
-		return random.choice(lines).strip()
+def display_text(stdscr,current,target, wpm=0):
+    stdscr.addstr(target)
 
-def wpm_test(stdscr):
-	target_text = load_text()
-	current_text = []
-	wpm = 0
-	start_time = time.time()
-	stdscr.nodelay(True)
+    for i,char in enumerate(current):
+            stdscr.addstr(char, curses.color_pair(2))
 
-	while True:
-		time_elapsed = max(time.time() - start_time, 1)
-		wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
 
-		stdscr.clear()
-		display_text(stdscr, target_text, current_text, wpm)
-		stdscr.refresh()
+def wpm_tester(stdscr):
+    target_text = "The quick brown fox jumps over the laxy dog."
+    current_text =[]
 
-		if "".join(current_text) == target_text:
-			stdscr.nodelay(False)
-			break
+    while True:
+        stdscr.clear()
+        display_text(stdscr,target_text,current_text)
+        stdscr.refresh()
+        key = stdscr.getkey()
 
-		try:
-			key = stdscr.getkey()
-		except:
-			continue
+        if ord(key)== 27:
+            break
+        if key in ("KEY_BACKSPACE", "\b","\x7f"):
+            if len(current_text)>0:
+                current_text.pop()
+        else:
+            current_text.append(key) 
 
-		if ord(key) == 27:
-			break
 
-		if key in ("KEY_BACKSPACE", '\b', "\x7f"):
-			if len(current_text) > 0:
-				current_text.pop()
-		elif len(current_text) < len(target_text):
-			current_text.append(key)
+
 
 
 def main(stdscr):
-	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(  1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(  2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(  3, curses.COLOR_GREEN, curses.COLOR_WHITE)
+   
+    start_screen(stdscr)
+ 
+    wpm_tester(stdscr)
+  
 
-	start_screen(stdscr)
-	while True:
-		wpm_test(stdscr)
-		stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
-		key = stdscr.getkey()
-		
-		if ord(key) == 27:
-			break
 
-wrapper(main)
+wrapper(main) # Wrapper call outside of the main function
+
+
